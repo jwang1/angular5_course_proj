9ea692e4b9a1c12bf0631b590512e8d92aed77e8
jwang1 <sigkdd@yahoo.com>
2018-01-01 17:23:52 -0500

    implementing recipe.model

--- src/app/model/recipe/recipe.model.ts
+++ src/app/model/recipe/recipe.model.ts
@@ -1,2 +1,11 @@
 export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, desc: string, ipath: string) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = ipath;
+  }
 }
