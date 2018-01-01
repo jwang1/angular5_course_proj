3aa537446af3f608b1cb23a6773e1fabd0841181
jwang1 <sigkdd@yahoo.com>
2018-01-01 14:59:21 -0500

    Set up the course project structure (schema) - project directory 1. based on :  https://www.udemy.com/the-complete-guide-to-angular-2/learn/v4/t/lecture/6656014?start=0
       basically,   root  (app-root, ie AppComponent)  Component
                    header Component  (I manually created it)
                    feature  (manually created this dir) which includes shopping-list and recipe-book features
                    shopping-list "feature" contains 2 components shopping-list and shopping-list-edit  (both were created with Angular-CLI, about CLI usage, copied the video below)
                    recipe-book "feature" contains 3 components: recipe-list, recipe-item, recipe-detail
                    "model" has 2 classes:  ingrediant and recipe
    
    2. This Venkat video is helpful (as a reference) is good; combined with this Maximillian course
         https://www.youtube.com/channel/UCCTVrRB5KpIiK6V2GGVsR1Q
    
    TODO:  will check the Maximillian stuff later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
 
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './feature/shopping-list/shopping-list/shopping-list.component';
+import { ShopingListEditComponent } from './feature/shopping-list/shoping-list-edit/shoping-list-edit.component';
+import { RecipeListComponent } from './feature/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './feature/recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './feature/recipe-book/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShopingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
